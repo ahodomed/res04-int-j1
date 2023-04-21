2ad56222d9f338d4f4ccadf7b322e75e047db0e7
ahodomed <ahodomed@gmail.com>
2023-04-21 14:34:05 +0000

    exercice j1

---
+++ article-wik.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>
+            Formulaire
+        </title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600;800&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css" />
+        
+    </head>
+   <body>
+       
+           <h1>Judith décapitant Holopherne (Artemisia Gentileschi)</h1>
+           <nav>
+               <ul>
+                    <li><a href="#"></a>Article</li>
+                    <li><a href="Discussion"></a>Article</li>
+                  
+               </ul>
+           </nav>
+           <main>
+               
+
+           <section>
+               
+           
+               <article>
+                   <em>Judith décapitant Holopherne</em> est le titre de deux tableaux de l'artiste peintre baroque italienne Artemisia
+                   Gentileschi, réalisés vers 1614-1620 conservés l'un au musée de Capodimonte à Naples et l'autre au musée
+                   
+                   <a href=""> des offices</a> à Florence.
+                   
+                   
+               </article>
+               <article>
+               <h2> Thème</h2>
+               <hr>
+               L’œuvre met en scène Judith qui tranche la tête du
+               général Holopherne. L’œuvre illustre une scène biblique
+               devenue courante dans le monde de la peinture depuis la Renaissance,
+               et qui fait partie d'un topos intitulé Pouvoir des femmes qui les montre dominant des hommes puissants. Tiré du livre de Judith des textes apocryphes 
+               bibliques, le tableau s'intéresse au moment où Judith, assistée 
+               de sa servante, décapite le général qui s'est endormi ivre.
+               </article>
+           </section>    
+           
+               
+     </main>
+          
+      
+      
+       
+       
+   </body>
+   <footer>
+       
+   </footer>
+        
